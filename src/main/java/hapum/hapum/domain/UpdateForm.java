79b6aeb72eccf23eb4fbf88c4ffa3d6a06f06348
leejh0315 <113866991+leejh0315@users.noreply.github.com>
2025-06-26 12:34:32 +0900

    Update UpdateForm.java

--- src/main/java/hapum/hapum/domain/UpdateForm.java
+++ src/main/java/hapum/hapum/domain/UpdateForm.java
@@ -9,6 +9,8 @@ public class UpdateForm {
 	private Long id;
 	
 	@NotBlank(message = "이름은 필수입니다.")
+	 @Size(min = 2, max = 30,
+          message = "이름은 최소 {min}자, 최대 {max}자까지 입력 가능합니다.")
 	private String name;
 
 	@Pattern(regexp = "^\\d{8}$", message = "생년월일은 8자리 숫자 (예: 20001225) 형식이어야 합니다.")
@@ -18,8 +20,12 @@ public class UpdateForm {
 	private String phone;
 
 	@NotBlank(message = "본당명을 입력해주세요.")
+	 @Size(min = 3, max = 50,
+          message = "본당명은 최소 {min}자, 최대 {max}자까지 입력 가능합니다.")
 	private String cathedral;
 
 	@NotBlank(message = "세례명을 입력해주세요.")
+	 @Size(min = 2, max = 30,
+          message = "세례명은 최소 {min}자, 최대 {max}자까지 입력 가능합니다.")
 	private String baptismName;
 }
